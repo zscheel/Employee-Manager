3ad37c9834470019d0632c58c5b7d86c16ade71d
zscheel <zachary.scheel1@gmail.com>
2023-05-30 14:55:47 -0500

    added tables to the shema.sql

--- Server.js
+++ Server.js
@@ -3,9 +3,9 @@ const mysql = require('mysql2');
 
 
 const db = mysql.createConnection({
-    host: 'localhost',
+    host: '127.0.0.1',
     user: 'root',
-    password: 'password',
+    password: 'zach2002',
     database: 'employee_db'
 });
 
---
+++ db/schema.sql
@@ -0,0 +1,21 @@
+USE employee_db;
+
+CREATE TABLE department (
+    id INTEGER AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(30) NOT NULL
+);
+
+CREATE TABLE role (
+    id INTEGER AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(30) NOT NULL,
+    department_id INTEGER,
+    salary DECIMAL(7)
+);
+
+CREATE TABLE employee (
+    id INTEGER AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INTEGER NOT NULL,
+    manager_id INTEGER
+);

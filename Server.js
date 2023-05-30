5967608c54b63f818490fedfdda64913dd9eb772
zscheel <zachary.scheel1@gmail.com>
2023-05-30 15:11:17 -0500

    added function to each user choice in the console

--- Server.js
+++ Server.js
@@ -18,6 +18,33 @@ const startQuestions = [
     }
 ]
 
+function responseHandler(data) {
+    if (data.choice === 'View All Employees') {
+        init();
+    }
+    if (data.choice === 'Add Employee') {
+        init();
+    }
+    if (data.choice === 'Update Employee Role') {
+        init();
+    }
+    if (data.choice === 'View All Roles') {
+        init();
+    }
+    if (data.choice === 'Add Role') {
+       init();
+    }
+    if (data.choice === 'View All Departments') {
+       init();
+    }
+    if (data.choice === 'Add Department') {
+        init();
+    }
+    if (data.choice === 'Quit') {
+        console.log('GoodBye');
+        process.exit(0);
+    }
+}
 
 
 function init() {
